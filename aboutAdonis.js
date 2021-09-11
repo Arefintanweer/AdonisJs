a16d0c9fa153766d31ae191535f7bbf713baa17d
Arefintanweer <arefintanweer@gmail.com>
2021-09-11 11:40:29 +0600

    Learning AdonisJs

---
+++ aboutAdonis.js
@@ -0,0 +1,23 @@
+/**
+ * whats make adonis unique?
+ * -its hardcore mvc pattern
+ * 
+ * what does mvc mean?
+ * A design patern.
+ * m = model - database interaction
+ * v = view - its the UI
+ * c = controller- handles all incoming requests,responsible communicating with model and loading views.
+ * 
+ * Use adonis as backend and just return JSON to use with vue/react.
+ * 
+ * npm init adonis-ts-app@latest hello-world - To create a new project
+ * node ace serve --watch
+ * adonis --help - To get all the commands 
+ * 
+ * extensions for vscode
+ * adonisjs
+ * edge template support
+ * 
+ * 
+ * 
+ */
